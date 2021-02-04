e278b8771cb908d9562d4488741c45b63bd6525a
makhijayogeshit1 <74236486+makhijayogeshit1@users.noreply.github.com>
2021-02-04 13:57:28 +0000

    made changes in learning java programe printed message

---
+++ learnJava.java
@@ -0,0 +1,10 @@
+
+public class learnJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("**** This is Java Program- created by yogesh*****");
+	}
+
+}
+
